4c6524b73b07e72d125def599112ea41c837fbcf
apark105 <andyjpark105@gmail.com>
2019-04-02 14:06:14 -0700

    refactored code

--- index.js
+++ index.js
@@ -3,52 +3,27 @@ window.onload = function(){
 }
 
 function init(){
-    document.getElementById("headers").addEventListener( "click", showTabInfo )
-    document.body.addEventListener( "click", closeTab )
+    const tab = document.querySelectorAll(".tab")
+    tab.forEach(selectTab => selectTab.addEventListener("click", changeTabColor));
 }
 
-function showTabInfo(){
-    const currentTab = event.target.innerText;
-    const current = event.target;
-    const removeTab = document.getElementById("headers").querySelectorAll('.tabClicked');    
-    const removeX = document.getElementById("infoTab").querySelectorAll('.showInfo');    
-    console.log('what is remove tab', removeTab[0])
-    let x;
-    switch(currentTab) {
-        case 'FIT GUIDE':
-            current.classList.add("tabClicked")
-            x = document.getElementsByClassName('fitGuide')
-            x[0].classList.add('showInfo')
-            removeX[0].classList.remove("showInfo")
-            removeTab[0].classList.remove("tabClicked")
-            break;
-        case 'CARE':
-            current.classList.add("tabClicked")
-            x = document.getElementsByClassName('care')
-            x[0].classList.add('showInfo')
-            removeX[0].classList.remove("showInfo")
-            removeTab[0].classList.remove("tabClicked")
-            break;
-        case 'MATERIALS':
-            current.classList.add("tabClicked")
-            x = document.getElementsByClassName('materials')
-            x[0].classList.add('showInfo')
-            removeX[0].classList.remove("showInfo")
-            removeTab[0].classList.remove("tabClicked")
-            break;
-        default:
-            break;
-    }
+function changeTabColor(){
+    closeTab()
+    const currentTab = event.target;
+    currentTab.classList.add("tabClicked")
+    showInfo(currentTab.id)
+}
+
+function showInfo(currentTab){
+    const info = document.getElementsByClassName(currentTab)
+    info[0].classList.add('showInfo')
 }
 
 function closeTab(){
-    const selection = event.target.id;
-    const removeTab = document.getElementById("headers").querySelectorAll('.tabClicked'); 
-    const removeX = document.getElementById("infoTab").querySelectorAll('.showInfo');       
-    if (selection !== 'tab' && selection !== 'infoTab') {
-        // debugger;
-        removeTab[0].classList.remove("tabClicked")
-        removeX[0].classList.remove("showInfo")
-
+    const removeTab = document.querySelectorAll('.tabClicked')[0]; 
+    const removeInfo = document.querySelectorAll('.showInfo')[0];  
+    if (removeTab) {
+        removeTab.classList.remove("tabClicked")
+        removeInfo.classList.remove("showInfo")
     }
 }
