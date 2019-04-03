b7c512924f8a1af31d46a5e6b98970d60a66e1e8
apark105 <andyjpark105@gmail.com>
2019-04-02 23:31:21 -0700

    added OOP

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
-window.onload = function(){
-    init();
-}
+// window.onload = function(){
+//     init();
+// }
 
-function init(){
-    const tab = document.querySelectorAll(".tab")
-    tab.forEach(selectTab => selectTab.addEventListener("click", changeTabColor));
-}
+// function init(){
+//     const tab = document.querySelectorAll(".tab")
+//     tab.forEach(selectTab => selectTab.addEventListener("click", changeTabColor));
+// }
 
-function changeTabColor(){
-    closeTab()
-    const currentTab = event.target;
-    currentTab.classList.add("tabClicked")
-    showInfo(currentTab.id)
-}
+// function changeTabColor(){
+//     closeTab()
+//     const currentTab = event.target;
+//     currentTab.classList.add("tabClicked")
+//     showInfo(currentTab.id)
+// }
 
-function showInfo(currentTab){
-    const info = document.getElementsByClassName(currentTab)
-    info[0].classList.add('showInfo')
-}
+// function showInfo(currentTab){
+//     const info = document.getElementsByClassName(currentTab)
+//     info[0].classList.add('showInfo')
+// }
 
-function closeTab(){
-    const removeTab = document.querySelectorAll('.tabClicked')[0]; 
-    const removeInfo = document.querySelectorAll('.showInfo')[0];  
-    if (removeTab) {
-        removeTab.classList.remove("tabClicked")
-        removeInfo.classList.remove("showInfo")
+// function closeTab(){
+//     const removeTab = document.querySelectorAll('.tabClicked')[0]; 
+//     const removeInfo = document.querySelectorAll('.showInfo')[0];  
+//     if (removeTab) {
+//         removeTab.classList.remove("tabClicked")
+//         removeInfo.classList.remove("showInfo")
+//     }
+// }
+
+class Tab {
+    init(){
+        const tab = document.querySelectorAll(".tab")
+        tab.forEach(selectTab => selectTab.addEventListener("click", this.changeTabColor));
     }
+    
+    changeTabColor = () => {
+        this.closeTab()
+        const currentTab = event.target;
+        currentTab.classList.add("tabClicked")
+        this.showInfo(currentTab.id)
+    }
+    
+    showInfo = (currentTab) => {
+        const info = document.getElementsByClassName(currentTab)
+        info[0].classList.add('showInfo')
+    }
+    
+    closeTab = () => {
+        const removeTab = document.querySelectorAll('.tabClicked')[0]; 
+        const removeInfo = document.querySelectorAll('.showInfo')[0];  
+        if (removeTab) {
+            removeTab.classList.remove("tabClicked")
+            removeInfo.classList.remove("showInfo")
+        }
+    }
+}
+
+window.onload = function(){
+    const individualTab = new Tab;
+    individualTab.init();
 }
