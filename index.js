29c80bf6ec4b42d9952c634caa4c404fd66ded00
apark105 <andyjpark105@gmail.com>
2019-04-03 16:45:51 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -3,25 +3,35 @@
 function Tab() {
   var _this = this;
 
+  /**
+   * function selects and attaches click handlers to all tabs
+   */
   this.init = function () {
     var tab = document.querySelectorAll(".tab");
     tab.forEach(function (selectTab) {
-      return selectTab.addEventListener("click", _this.changeTabColor);
+      selectTab.addEventListener("click", _this.toggleTab);
     });
-  }, 
-
-  this.changeTabColor = function () {
+  },
+  /**
+   * function toggles between tabs
+   */
+  this.toggleTab = function () {
     _this.closeTab();
     var currentTab = event.target;
     currentTab.classList.add("tabClicked");
     _this.showInfo(currentTab.id);
-  }, 
-
+  },
+  /** 
+   * function displays content based on the current tab
+   * @param {string} currentTab - name of the tab clicked
+   */
   this.showInfo = function (currentTab) {
     var info = document.getElementsByClassName(currentTab)[0];
     info.classList.add('showInfo');
-  }, 
-
+  },
+  /**
+   * function closes previously opened tab 
+   */
   this.closeTab = function () {
     var removeTab = document.querySelectorAll('.tabClicked')[0];
     var removeInfo = document.querySelectorAll('.showInfo')[0];
